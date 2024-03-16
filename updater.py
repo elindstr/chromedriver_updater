87d0383a24be3179163c395f37f418898bef718b
elindstr <elindstr@gmail.com>
2024-03-15 20:50:52 -0700

    convert to top-level version

--- updater.py
+++ updater.py
@@ -14,6 +14,10 @@ PATH = '/Users/elindstr/bin/chromedriver'
 command = '/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --version'
 VERSION = subprocess.check_output(command, shell=True).decode()
 VERSION = VERSION.split()[-1].strip() 
+print(f'Current local chrome version (full): {VERSION}')
+
+VERSIONtop = VERSION.split('.')[0]
+print(f'Current local chrome version (top-level): {VERSIONtop}')
 
 ## Get local chrome version (windows)
 # command = 'reg query "HKEY_CURRENT_USER\\Software\\Google\\Chrome\\BLBeacon" /v version'
@@ -33,25 +37,26 @@ data = response.json()
 
 # Locate driver download link based on parameters
 download_url = ""
-for version in data['versions']:
-    if VERSION in version['version']:
-        for download in version['downloads'][PACKAGE]:
-            if download['platform'] == PLATFORM:
-                download_url = download['url']
-                break
+for version in reversed(data['versions']):
+    if VERSIONtop in version['version'].split('.')[0]:
+        if PACKAGE in version['downloads']:
+            for download in reversed(version['downloads'][PACKAGE]):
+                if download['platform'] == PLATFORM:
+                    download_url = download['url']
+                    break
     if download_url:
         break
 
 # Download driver to PATH
 if not download_url:
-    print(f"No matching chromedriver found for Chrome version {VERSION}.")
+    print(f"No matching chromedriver found for Chrome version {VERSIONtop}.")
 else:
     response = requests.get(download_url, stream=True)
     response.raise_for_status()
     with open(PATH, 'wb') as file:
         for chunk in response.iter_content(chunk_size=8192):
             file.write(chunk)
+    print(f'Updated chromedriver to {VERSIONtop}.')
 
-print(f'Updated chromedriver to {VERSION}.')
 
     
